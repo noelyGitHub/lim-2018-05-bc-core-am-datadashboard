58a5f512901ec0e249ecbeb09dbafa50cda1e27b
noelyGitHub <noely.alis209@gmail.com>
2018-06-08 12:20:36 -0500

    intentando conectar con json

--- src/data.js
+++ src/data.js
@@ -1,4 +1,15 @@
-window.computeUsersStatus(users,progress,coirses);
+let ourRequest=new XMLHttpRequest();
+ourRequest.open('GET','../data/cohorts.json');
+ourRequest.onprogress=function(){
+    console.log(ourRequest.statusText);
+}
+ourRequest.onload = function () {
+    console.log(ourRequest.responseText);
+    //console.log(XMLHttpRequest.status);
+}
+ourRequest.send(null);
+
+/*window.computeUsersStatus(users,progress,coirses);
 window.SortUsers(users,orderBy,orderDirecction);// retorna un arreglo ordenado
 window.filterUsers(users,search);
-window.processCohortData(options);
+window.processCohortData(options);*/
