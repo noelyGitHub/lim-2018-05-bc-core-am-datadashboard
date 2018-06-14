e04cc12c839a97838fa1045e859ddd1d135a22bb
noelyGitHub <noely.alis209@gmail.com>
2018-06-14 13:01:45 -0500

    revisir datos crudos en la primera funcion

--- src/data.js
+++ src/data.js
@@ -1,63 +1,56 @@
-//................................XHR
-let xhr=new XMLHttpRequest();// instancio el objeto
-xhr.open('GET','http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json',true);// hago los pedidos a mi servidor GET/POST -> solicitar datos/solicitar y enviar
-xhr.onload =_=> {
-    // CORS controles de acceso a servidores cruzados para servidores web
-    if (xhr.readyState=== 4 && xhr.status === 200) {// Success!
-        let data = JSON.parse(xhr.responseText);
-       //console.log(data); // data en object
-        for(var i in data){
-            if(data[i].name=="Noely"){
-                console.log(data[i]);
+    let url1='http://localhost:8080/data/cohorts.json';
+    let url2='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+    let url3='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json';
+
+const connectJson=(url,callback)=>{
+    var xmlhttp=new XMLHttpRequest(); 
+    let dateJson;
+    xmlhttp.onload=_=>{
+            if(xmlhttp.readyState === 4){
+                if(xmlhttp.status !== 200){
+                    return callback(new Error(`HTTP error: ${xmlhttp.status}`));
+                }
+                try {
+                  callback(null, JSON.parse(xmlhttp.responseText));// devuelve una funcion con dos parametros??
+                } catch (err) {
+                  callback(err);
+                }
             }
-            
-           //console.log(Object.getOwnPropertyNames(data[i]));
-            //console.log(data.publicAdmission);// me lista las propiedades del object data
-        }
-       
-            
-      }   
+    };    
+    xmlhttp.open('GET',url);
+    xmlhttp.send();   
+};
+var getJSON = (urlJSON) => connectJson(urlJSON,(error,json) => {
+    if(error){
+        return console.error(error);
+    }
+    console.log(json);
+    //let courses=json.coursesIndex;
+    //let idCourses=Object.getOwnPropertyNames(courses);
+  
+    // json.map(function(){return json.id;}); 
+});
+getJSON(url1);
+//...................................................obtener users 
+let users=_=>{
+    return getJSON(url3);
 }
-xhr.onerror=_=>console.log("error");// Error!
-xhr.send();
-/*
-// ................................FETCH
-fetch('http://localhost:8080/data/cohorts.json').then((response)=>{      
-         if(response.status==200){
-             console.log(response.status);
-             console.log(response.json());
-             console.log("SUCCESFULL holaa");
-         } 
-}).catch((error)=>{
-    console.log("Uy! fallaste en algo :(");
+//...................................................Obtener Progress
+let progress=_=>{
+    return getJSON(url2);
+}
+//...................................................Obtener courses
+let courses=_=>{
+    let cohortDate=getJSON(url1);
+    let courses=cohortDate.coursesIndex;    
+    for(var i in cohortDate){
+       let idCourses=Object.keys(courses);  
+    }
+}
+//..........................................................................................computeUsersstats
+var computeUsersStats=(users,progress,courses) => {// creara lista usuarios
 
-});
-//................................FETCH
-const llamadasAjax=[];
-llamadasAjax.push(fetch('http://localhost:8080/data/cohorts.json'));
-llamadasAjax.push(fetch('http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json'));
-llamadasAjax.push(fetch('http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json'));
-
-Promise.all(llamadasAjax).then((responses)=>{
-        console.log(responses[0].json());
-        console.log(responses[1].json());
-        console.log(responses[2].json());
-        console.log("provando promises");
-   
-}).catch((error)=>{
-    console.log("errorss")
-});
-*/
-/*.................................................................................................. */
-var computeUsersStats=(ArrayUsers,ObjectProgress,ArrayCourses)=>{// creara lista usuarios
-    /*users->arreglo de objetos
-    progress->objeto de progreso del usuario para cada curso
-    courses->arreglo de ids de los cursos del cohort de 'coursesIndex'
-    Recorrer el arreglo de usuario y calcular los indcadores
-    devolver un nuevo arreglo usuario 
-
-    */
-       let stats={
+    let stats={
         percent: 0.8,//% de completitudgeneral --> stats
         excerses:{
             total:15,// # total de ejercicios autocorregidos en cursos
@@ -74,70 +67,9 @@ var computeUsersStats=(ArrayUsers,ObjectProgress,ArrayCourses)=>{// creara lista
             completed:6,// # de quizzes completados por usuarios
             percent:0.4, // % de quizzes completados
             scoreSum:350,// suma de puntuaciones de los quizzes
-            scoreAvg:13,// promedio de puntuaciones en quizzes
+            scoreAvg:13// promedio de puntuaciones en quizzes
         }
      };
-     usersWithStats=[stats.percent,stats.excerses,stats.reads,stats.quizzes]
-    return usersWithStats;
-    
+    usersWithStats=[stats.percent,stats.excerses,stats.reads,stats.quizzes]
+    return usersWithStats;  
 };
-var sortUsers=(arrayUsers,orderBy,orderDirecction)=>{
-// ordenar la lista
-// users: arreglo de obj creado con computeUserStats
-// computeUsersStats();
-// orderBy: criterio de ordenado nombre,% de completitud,% ejercicios, quizzes,promedio y lecturas
-// orderDirecction ACS y DES
-//..quiero que mis criterios esten en un array
-var criterio=[0,1,2,3,4,5,6];
-for(i=0;i<=criterio.lenght;i++){
-    if(orderBy==i){
-        if(orderDirecction=='ASC'){
-            //algoritmo para ordenar utilizando computerUsersStats
-        arrayOrdenado=[/*..*/];
-        }
-    }
-
-}
-return arrayOrdenado;
-// arreglo de usuario ordenado
-};
-var filter=(arrayUsers,search)=>{
-//users: arreglo computerUsersStats
-// string de busqueda
-
-    for(i=0;i<=computeUsersStats.users.lenght;i++){
-        //
-        var names=computeUsersStats.users[i].name[i];
-        if(search==names){
-            return computeUsersStats.users[i];// tiene que retornar las alumnas con monbre noe
-        }
-    }
-
-}
-
-var processCohortData=(options)=>{
-
-}
-/*
-window.SortUsers(users,orderBy,orderDirecction);// retorna un arreglo ordenado
-w
-window.processCohortData(options);*/
-window.computeUsersStats;
-window.sortUsers;
-window.filterUsers;
-
-//................................LLAMADA FORMULARIO
-function llamarAjax(){
-   
-	let xhrm=new XMLHttpRequest();
-    xhrm.open('GET','mensaje.php',true);
-    xhrm.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xhrm.onload=_=>{
-        if(xhrm.readyState === 4 && xhrm.status === 200){
-            console.log("accedio ");
-		    document.getElementById("mensajes").innerHTML=xhrm.responseText;
-        }        
-}
-	xhrm.onerror=_=>console.log("hubo un errror");	
-	xhrm.send();
-}
