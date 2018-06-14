dcf9d24bdd768abb9b8527c3846372d169e72435
noelyGitHub <noely.alis209@gmail.com>
2018-06-14 19:05:19 -0400

    funcion retorna con fetch

--- src/data.js
+++ src/data.js
@@ -1,62 +1,96 @@
+
+let url1='http://localhost:8080/data/cohorts.json';
+    let url2='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+    let url3='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json';
+function flo(url){
+    return fetch(url).then((responses)=>{
+        let dateJson= responses.json(); 
+        let result=[];
+        let prueva;
+        prueva=Object.getOwnPropertyNames(dateJson);
+        for(var i in dateJson){
+            result+=dateJson[i];
+            
+        }
+        return prueva;
+        //En caso de que sean llamadas a api
+    });    
+}
+console.log(flo(url2));
+/*
 //................................XHR
+let cohortJson='http://localhost:8080/data/cohorts.json';
+let progressJson='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+let usersJson='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json';
+
 let xhr=new XMLHttpRequest();// instancio el objeto
-xhr.open('GET','http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json',true);// hago los pedidos a mi servidor GET/POST -> solicitar datos/solicitar y enviar
+xhr.open('GET',progressJson,true);// hago los pedidos a mi servidor GET/POST -> solicitar datos/solicitar y enviar
 xhr.onload =_=> {
     // CORS controles de acceso a servidores cruzados para servidores web
     if (xhr.readyState=== 4 && xhr.status === 200) {// Success!
         let data = JSON.parse(xhr.responseText);
        //console.log(data); // data en object
         for(var i in data){
-            if(data[i].name=="Noely"){
-                console.log(data[i]);
-            }
-            
-           //console.log(Object.getOwnPropertyNames(data[i]));
+            //console.log(data[i]);
+                
+            //console.log(Object.getOwnPropertyNames(data[i].coursesIndex));
             //console.log(data.publicAdmission);// me lista las propiedades del object data
-        }
-       
-            
+        }      
       }   
 }
+
 xhr.onerror=_=>console.log("error");// Error!
 xhr.send();
-/*
-// ................................FETCH
-fetch('http://localhost:8080/data/cohorts.json').then((response)=>{      
-         if(response.status==200){
-             console.log(response.status);
-             console.log(response.json());
-             console.log("SUCCESFULL holaa");
-         } 
-}).catch((error)=>{
-    console.log("Uy! fallaste en algo :(");
+function con(url){
+   fetch(url).then((response)=>{
+    if(response.status == 200){
+        return response.json();
+    }else{
+        throw new Error("La llamada a la API falló");
+    }
+}); 
+}
+var n=con(cohortJson);
+console.log(n);
 
-});
-//................................FETCH
-const llamadasAjax=[];
-llamadasAjax.push(fetch('http://localhost:8080/data/cohorts.json'));
-llamadasAjax.push(fetch('http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json'));
-llamadasAjax.push(fetch('http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json'));
 
-Promise.all(llamadasAjax).then((responses)=>{
-        console.log(responses[0].json());
-        console.log(responses[1].json());
-        console.log(responses[2].json());
-        console.log("provando promises");
-   
-}).catch((error)=>{
-    console.log("errorss")
-});
-*/
+
+connectJson=(url)=>{
+    var xmlhttp=new XMLHttpRequest(); 
+    try{
+        let dateJson;
+        xmlhttp.open('GET',url);
+        xmlhttp.onload=_=>{
+            if(xmlhttp.readyState===4 && xmlhttp.status === 200){
+                return JSON.parse(xmlhttp.responseText);
+            }        
+        }    
+        xmlhttp.send();
+        //return dateJson;
+    }catch(e){
+        return ("Error: " + e.description);
+    }    
+}
+let cohortJson='http://localhost:8080/data/cohorts.json';
+
+var  viewObject= connectJson(cohortJson);
+console.log(viewObject);
+
 /*.................................................................................................. */
-var computeUsersStats=(ArrayUsers,ObjectProgress,ArrayCourses)=>{// creara lista usuarios
-    /*users->arreglo de objetos
-    progress->objeto de progreso del usuario para cada curso
-    courses->arreglo de ids de los cursos del cohort de 'coursesIndex'
-    Recorrer el arreglo de usuario y calcular los indcadores
-    devolver un nuevo arreglo usuario 
+let progress=()=>{
+    let progress=connectJson(progressJson);
+    let users=connectJson(usersJson);
+    for(let i in progress){
 
-    */
+    }
+    return uid,{'javascriitp':'80'}
+    //retornar llave para cada usuario (uid) con un objeto que contiene el progreso del usuario para cada curso.
+}
+let courses=()=>{
+    // Arreglo de strings con los ids de los cursos del cohort en cuestión.
+    //Esta data se puede extraer de la propiedad coursesIndex de los objetos que representan los cohorts.
+}
+var computeUsersStats=(users,progress,courses)=>{// creara lista usuarios
        let stats={
         percent: 0.8,//% de completitudgeneral --> stats
         excerses:{
