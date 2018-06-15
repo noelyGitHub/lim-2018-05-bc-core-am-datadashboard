51ead0136ba76ebb65ce60d87b8b470a111c1e96
noelyGitHub <noely.alis209@gmail.com>
2018-06-15 00:19:42 -0400

    terminado la preparacion de datos

--- src/data.js
+++ src/data.js
@@ -1,99 +1,3 @@
-<<<<<<< HEAD
-
-let url1='http://localhost:8080/data/cohorts.json';
-    let url2='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-    let url3='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json';
-function flo(url){
-    return fetch(url).then((responses)=>{
-        let dateJson= responses.json(); 
-        let result=[];
-        let prueva;
-        prueva=Object.getOwnPropertyNames(dateJson);
-        for(var i in dateJson){
-            result+=dateJson[i];
-            
-        }
-        return prueva;
-        //En caso de que sean llamadas a api
-    });    
-}
-console.log(flo(url2));
-/*
-//................................XHR
-let cohortJson='http://localhost:8080/data/cohorts.json';
-let progressJson='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-let usersJson='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json';
-
-let xhr=new XMLHttpRequest();// instancio el objeto
-xhr.open('GET',progressJson,true);// hago los pedidos a mi servidor GET/POST -> solicitar datos/solicitar y enviar
-xhr.onload =_=> {
-    // CORS controles de acceso a servidores cruzados para servidores web
-    if (xhr.readyState=== 4 && xhr.status === 200) {// Success!
-        let data = JSON.parse(xhr.responseText);
-       //console.log(data); // data en object
-        for(var i in data){
-            //console.log(data[i]);
-                
-            //console.log(Object.getOwnPropertyNames(data[i].coursesIndex));
-            //console.log(data.publicAdmission);// me lista las propiedades del object data
-        }      
-      }   
-}
-
-xhr.onerror=_=>console.log("error");// Error!
-xhr.send();
-function con(url){
-   fetch(url).then((response)=>{
-    if(response.status == 200){
-        return response.json();
-    }else{
-        throw new Error("La llamada a la API falló");
-    }
-}); 
-}
-var n=con(cohortJson);
-console.log(n);
-
-
-
-connectJson=(url)=>{
-    var xmlhttp=new XMLHttpRequest(); 
-    try{
-        let dateJson;
-        xmlhttp.open('GET',url);
-        xmlhttp.onload=_=>{
-            if(xmlhttp.readyState===4 && xmlhttp.status === 200){
-                return JSON.parse(xmlhttp.responseText);
-            }        
-        }    
-        xmlhttp.send();
-        //return dateJson;
-    }catch(e){
-        return ("Error: " + e.description);
-    }    
-}
-let cohortJson='http://localhost:8080/data/cohorts.json';
-
-var  viewObject= connectJson(cohortJson);
-console.log(viewObject);
-
-/*.................................................................................................. */
-let progress=()=>{
-    let progress=connectJson(progressJson);
-    let users=connectJson(usersJson);
-    for(let i in progress){
-
-    }
-    return uid,{'javascriitp':'80'}
-    //retornar llave para cada usuario (uid) con un objeto que contiene el progreso del usuario para cada curso.
-}
-let courses=()=>{
-    // Arreglo de strings con los ids de los cursos del cohort en cuestión.
-    //Esta data se puede extraer de la propiedad coursesIndex de los objetos que representan los cohorts.
-}
-var computeUsersStats=(users,progress,courses)=>{// creara lista usuarios
-       let stats={
-=======
     let url1='http://localhost:8080/data/cohorts.json';
     let url2='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
     let url3='http://localhost:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json';
@@ -121,33 +25,33 @@ var getJSON = (urlJSON) => connectJson(urlJSON,(error,json) => {
         return console.error(error);
     }
     console.log(json);
-    //let courses=json.coursesIndex;
-    //let idCourses=Object.getOwnPropertyNames(courses);
-  
-    // json.map(function(){return json.id;}); 
+   return json;
 });
 getJSON(url1);
 //...................................................obtener users 
-let users=_=>{
+let users =_=> {
     return getJSON(url3);
 }
 //...................................................Obtener Progress
-let progress=_=>{
+let progress =_=> {
     return getJSON(url2);
 }
 //...................................................Obtener courses
-let courses=_=>{
-    let cohortDate=getJSON(url1);
-    let courses=cohortDate.coursesIndex;    
-    for(var i in cohortDate){
-       let idCourses=Object.keys(courses);  
-    }
+let courses =_ => {
+    let cohortDate=getJSON(url1);    
+    let arrayIds=[];
+    for(var i in cohortDate){// recorro todo el objeto cohor
+        var idCourses=Object.keys(json[i].coursesIndex);  //obtengo las propiedades del objeto coursesIndex
+        for(var value of idCourses){// voy a recoger el objetos de propiedadess para crear un nuevo array con sus valores K-> paropiedad
+            arrayIds.push(value);// agregar todos los datos encontrados
+         }
+        return arrayIds;// devuelve arreglo de ids del curso pero estan repetidos;
+     }
 }
 //..........................................................................................computeUsersstats
-var computeUsersStats=(users,progress,courses) => {// creara lista usuarios
+var computeUsersStats = (users,progress,courses) => {// creara lista usuarios
 
     let stats={
->>>>>>> e04cc12c839a97838fa1045e859ddd1d135a22bb
         percent: 0.8,//% de completitudgeneral --> stats
         excerses:{
             total:15,// # total de ejercicios autocorregidos en cursos
@@ -170,3 +74,17 @@ var computeUsersStats=(users,progress,courses) => {// creara lista usuarios
     usersWithStats=[stats.percent,stats.excerses,stats.reads,stats.quizzes]
     return usersWithStats;  
 };
+function llamarAjax(){
+   
+	let xhrm=new XMLHttpRequest();
+    xhrm.open('GET','mensaje.php',true);
+    xhrm.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xhrm.onload=_=>{
+        if(xhrm.readyState === 4 && xhrm.status === 200){
+            console.log("accedio ");
+		    document.getElementById("mensajes").innerHTML=xhrm.responseText;
+        }        
+}
+	xhrm.onerror=_=>console.log("hubo un errror");	
+	xhrm.send();
+}
